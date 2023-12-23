fe70dcee584c1683f20eb9dfdfb25d70f0ffc433
Bamcane <diandaokui@qq.com>
2023-12-23 14:44:46 +0800

    修复

--- js/pages.js
+++ js/pages.js
@@ -1,10 +1,10 @@
 
 function AddHead(Page) {
+    document.getElementById("head").innerHTML += '<base href="https://bamcane.github.io/" target="_blank">'
     document.getElementById("head").innerHTML += '<link rel="stylesheet" type="text/css" href="style.css"/>'
     document.getElementById("head").innerHTML += '<link rel="shortcut icon" href="bamcane.png">'
     document.getElementById("head").innerHTML += '<meta charset="utf-8">'
     document.getElementById("head").innerHTML += '<title>甘蔗边上的竹林 - ' + Page + '</title>'
-    document.getElementById("head").innerHTML += '<base href="https://bamcane.github.io/" target="_blank">'
 }
 
 function AddPageList() {
